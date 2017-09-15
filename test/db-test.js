f84798d2959026d845e2494c12f49078451a1381
enmisac <enmisac@gmail.com>
2017-09-14 19:22:00 -0600

    agregue pruebas para testar la conexiona a base de datos

--- test/db-test.js
+++ test/db-test.js
@@ -1,12 +1,34 @@
 'use strict'
 
 const test = require('ava')
-const uuid = require('uuid-base64')
+const uuid = require('uuid-base62')
 const Db = require('../')
+const r = require('rethinkdb')
 
 const dbName = `platzigram_${uuid.v4()}`
 const db = new Db({ db: dbName })
 
+test.before('setup database', async t => {
+  await db.connect()
+  t.true(db.connected, 'should be connected')
+})
+
+test.after('disconnect database', async t => {
+  await db.disconnect()
+  t.false(db.connected, 'should be disconnected')
+})
+
+test.after.always('cleanup database', async t => {
+  let conn = await r.connect({})
+  await r.dbDrop(dbName).run(conn)
+})
+
 test('save image', async t => {
-	
+  t.is(typeof db.saveImage, 'function', 'saveImage is function')
+  
+  let image = {
+    url: `https://platzigram.test/${uuid.v4()}.jpg`,
+    likes: 0,
+    user_id: uuid.uuid()
+  }
 })
