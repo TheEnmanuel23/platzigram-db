87a226d88bba8eaef4008bfc50323ecfb75ec529
enmisac <enmisac@gmail.com>
2017-09-14 22:00:00 -0600

    agregue la funcion prara guardar likes de imagenes asi como su respectivo test

--- test/db-test.js
+++ test/db-test.js
@@ -4,6 +4,7 @@ const test = require('ava')
 const uuid = require('uuid-base62')
 const Db = require('../')
 const r = require('rethinkdb')
+const fixtures = require('./fixtures')
 
 const dbName = `platzigram_${uuid.v4()}`
 const db = new Db({ db: dbName })
@@ -26,18 +27,27 @@ test.after.always('cleanup database', async t => {
 test('save image', async t => {
   t.is(typeof db.saveImage, 'function', 'saveImage is function')
 
-  let image = {
-    url: `https://platzigram.test/${uuid.v4()}.jpg`,
-    likes: 0,
-    liked: false,
-    user_id: uuid.uuid()
-  }
+  let image = fixtures.getImage()
 
   let created = await db.saveImage(image)
+  t.is(created.description, image.description)
   t.is(created.url, image.url)
   t.is(created.likes, image.likes)
   t.is(created.liked, image.liked)
+  t.deepEqual(created.tags, [ 'awesome', 'tags', 'platzi' ])
   t.is(created.user_id, image.user_id)
   t.is(typeof created.id, 'string')
+  t.is(created.public_id, uuid.encode(created.id))
   t.truthy(created.createdAt)
+})
+
+test('like image', async t => {
+  t.is(typeof db.likeImage, 'function', 'likeImage is a function')
+
+  let image = fixtures.getImage()
+  let created = await db.saveImage(image)
+  let result = await db.likeImage(created.public_id)
+
+  t.true(result.liked)
+  t.is(result.likes, image.likes + 1)
 })
